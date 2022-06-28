fa1e9bb026814f296b96771d43d48a73732b06e6
Anti Coding Club <73062879+Thoybur-Rohman@users.noreply.github.com>
2022-06-28 13:27:52 +0100

    added routing and dummy data,

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require("express");
 
 const app = express();
 const db = require("./db.js");
+const jobsRoute = require('./Roots/jobsRoute')
+
+app.use('/api/jobs/' , jobsRoute)
+
+
+
 const port = process.env.PORT || 5000;
 
 app.listen(port,() => console.log("Server is running"));
